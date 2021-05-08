04ad60a5b1760bb48620240b5376ec28c934e42a
Michael Tichý <m.tichy22@seznam.cz>
2021-05-08 13:04:32 +0200

    App: made app to work

---
+++ src/Controller/HomePageController.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types = 1);
+
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomePageController extends AbstractController
+{
+
+	public function index(): Response {
+		return new Response('AAA');
+	}
+}
