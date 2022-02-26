528ab3f02b7e8d3a7c133534b0522bc3dcb87f51
Michael Tichý <m.tichy22@seznam.cz>
2022-02-26 19:18:22 +0100

    App: moved fontawesome to webpack

--- assets/app.js
+++ assets/app.js
@@ -10,6 +10,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
+//fontawesome
+import '@fortawesome/fontawesome-free/css/all.css'
+
 //custom JS
 import Flashes from './js/Flashes.js';
 
