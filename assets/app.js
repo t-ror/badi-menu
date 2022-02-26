543207524f977b2521a7ad97fd15164c2b6f6657
Michael Tichý <m.tichy22@seznam.cz>
2022-02-26 18:14:33 +0100

    App: moved boostrap-icons under webpack

--- assets/app.js
+++ assets/app.js
@@ -4,3 +4,4 @@ import './styles/app.css';
 //bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.css'
